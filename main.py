cfba06201e40f1729f2b143a848ae8ad7e8445ed
EdanTuti <edantuti@gmail.com>
2020-09-11 12:49:33 +0000

    Made some really cool changes

--- main.py
+++ main.py
@@ -42,12 +42,14 @@ async def on_command_error(ctx, error):
 #just to check whether the bot is good
 @bot.command()
 async def hello(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Hi!", type=ActivityType.custom))
     embed=Embed(title="hello", color=Color.green())
     await ctx.send(embed=embed)
 
 #Command that actually starts the server
 @bot.command()
 async def start(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Starting the Server", type=ActivityType.custom))
     server.StartServer()
     if server.GetStatus() == 'Online':
         embed = Embed(title="Bhau server online hai", color=Color.blue())
@@ -62,6 +64,7 @@ async def start(ctx):
 #Command that stops the server
 @bot.command()
 async def stop(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Stopping the Server", type=ActivityType.custom))
     embed=Embed(title=server.StopServer(), color=Color.blue())
     await ctx.send(embed=embed)
     await ctx.send(str(ctx.author.mention))
@@ -69,6 +72,7 @@ async def stop(ctx):
 #Command that gives of the server
 @bot.command()
 async def status(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Sending Server Status", type=ActivityType.custom))
     embed=Embed(title=server.GetStatus(), color=Color.blue())
     await ctx.send(embed=embed)
     await ctx.send(ctx.author.mention)
@@ -76,6 +80,7 @@ async def status(ctx):
 #Command which gives random headlines from the Indian news pages
 @bot.command()
 async def headlines(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Sending News Headlines", type=ActivityType.custom))
     data = requests.get(url="https://newsapi.org/v2/top-headlines?country=in&apiKey="+api).json()
     headlines = data["articles"]
     number = randint(0, len(headlines)-1)
@@ -87,6 +92,7 @@ async def headlines(ctx):
 #Command which provides news for particular topic
 @bot.command()
 async def sources(ctx, arg):
+    await bot.change_presence(activity=CustomActivity(name="Sending News", type=ActivityType.custom))
     data = requests.get(url="https://newsapi.org/v2/everything?q="+arg+"&apiKey="+api).json()
     sources = data['articles']
     number=randint(0, len(sources)-1)
@@ -98,6 +104,7 @@ async def sources(ctx, arg):
 #Command which provides all the commands that are present in the code
 @bot.command()
 async def help(ctx):
+    await bot.change_presence(activity=CustomActivity(name="Sending help", type=ActivityType.custom))
     embed=Embed(title='Helps/commands of the '+(bot.user.name), description="This Bot is made By Edantuti.",color=Color.dark_blue())
     embed.set_author(name="Developer:Edan Solomon Tuti", url='https://github.com/Edantuti/DiscordBotYOLO')
     embed.add_field(name='News:', value='The command for news:\na!headlines-To show the top headlines in India.\na!sources <query>-To get the news for specific topic.', inline=True)
